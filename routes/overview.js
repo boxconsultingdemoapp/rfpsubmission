c1e62078b3aeb37aed83430d17a4199772d24bb8
Josh Rael <jrael@mba-003013.local>
2016-10-26 23:43:04 -0500

    update stylesheet to look more like box

--- routes/overview.js
+++ routes/overview.js
@@ -3,12 +3,14 @@ let express = require('express');
 let router = express.Router();
 
 let AppConfig = require('../config').AppConfig;
+let BoxConfig = require('../config').BoxConfig;
 
 /* GET overview page. */
 router.get('/', function (req, res, next) {
 	req.app.locals.boxAdminApiClient.folders.get('0', null, function(err, response){
 		console.log(response);
 	});
+	// boxClient.files.getEmbedLink({ id: fileId })
 	res.render('pages/overview', { title: "RFP", domain: AppConfig.domain });
 });
 
